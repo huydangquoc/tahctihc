ecab4a9c59d0d62a50a8446c80b9b66e5a3acc7a
Dang Quoc Huy <huysamdua@yahoo.com>
2016-10-20 22:37:20 +0700

    support login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	def index
+		Rails.logger.info request.env["HTTP_COOKIE"]
 		@users = User.all
 	end
 
@@ -10,6 +11,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new user_params
 		if @user.save
+			session[:user_id] = @user.id
 			redirect_to root_path, notice: "Account created."
 		else
 			render 'new'
